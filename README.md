edc9815908fab89889f5986275bc296cebf0c4a6
Julian Hecker <julianheckerdev@gmail.com>
2021-01-21 13:15:33 -0500

    Initial Commit: Create Gatsby App

---
+++ README.md
@@ -0,0 +1,2 @@
+# [julianhecker.dev](https://julianhecker.dev)
+Julian Hecker's personal portfolio site built using React and Gatsby.
---
+++ gatsby-config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  siteMetadata: {
+    title: "julianhecker.dev",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "G-HDX5HKGK2W",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-transformer-remark",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};

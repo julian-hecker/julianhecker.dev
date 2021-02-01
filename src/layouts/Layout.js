2f025b5067256e4bc2440a299c5b8e4508f77672
Julian Hecker <julianheckerdev@gmail.com>
2021-01-31 22:09:29 -0500

    add favicon

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 
 import Footer from './Footer';
 import Header from './Header';
 
 import '../styles/index.scss';
+import favicon from '../assets/icons/favicon.png'
 
 const Layout = ({ children }) => (
     <>
+        <Helmet>
+            <link rel="icon" href={favicon} />
+        </Helmet>
         <Header />
         <main>{children}</main>
         <Footer />

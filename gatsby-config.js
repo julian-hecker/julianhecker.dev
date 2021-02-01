8a29a37db20e06460fabd6091a3c9e75a82b91e3
Julian Hecker <julianheckerdev@gmail.com>
2021-01-31 20:40:19 -0500

    add google fonts plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,36 +1,43 @@
 module.exports = {
-  siteMetadata: {
-    title: "julianhecker.dev",
-    siteUrl: "https://julianhecker.dev"
-  },
-  plugins: [
-    "gatsby-plugin-sass",
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "UA-187787321-1",
-      },
+    siteMetadata: {
+        title: 'julianhecker.dev',
+        siteUrl: 'https://julianhecker.dev',
     },
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-transformer-remark",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
-  ],
+    plugins: [
+        'gatsby-plugin-sass',
+        {
+            resolve: 'gatsby-plugin-google-analytics',
+            options: {
+                trackingId: 'UA-187787321-1',
+            },
+        },
+        'gatsby-plugin-sharp',
+        'gatsby-plugin-react-helmet',
+        'gatsby-plugin-sitemap',
+        'gatsby-transformer-remark',
+        'gatsby-transformer-sharp',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'images',
+                path: './src/images/',
+            },
+            __key: 'images',
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'pages',
+                path: './src/pages/',
+            },
+            __key: 'pages',
+        },
+        {
+            resolve: `gatsby-plugin-google-fonts`,
+            options: {
+                fonts: [`Roboto`, `Roboto Mono`],
+                display: `swap`,
+            },
+        },
+    ],
 };

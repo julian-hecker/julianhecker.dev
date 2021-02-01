629414d8165c96c9c994b9132c061070d87e9f6b
Julian Hecker <julianheckerdev@gmail.com>
2021-01-31 20:48:25 -0500

    create resume page

---
+++ src/pages/resume.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import resume from '../assets/Julian_Hecker_Developer.pdf';
+
+function Resume() {
+    return (
+        <iframe
+            src={resume}
+            title="Julian Hecker Web Development Resume"
+            width="100%"
+            style={{
+                display: 'block',
+                height: '100vh',
+                border: 'none',
+            }}
+        ></iframe>
+    );
+}
+
+export default Resume;

3d7b4e3ed4021d10a6701108123dd2cced69532f
Julian Hecker <julianheckerdev@gmail.com>
2021-06-26 01:36:41 -0400

    update resume

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
 import '../styles/index.scss';
-import resume from '../assets/Julian_Hecker_Developer.pdf';
+import resume from '../assets/Julian_Hecker_Resume_June.pdf';
 
 function Resume() {
-    return (
-        <iframe
-            src={resume}
-            title="Julian Hecker Web Development Resume"
-            width="100%"
-            style={{
-                display: 'block',
-                height: '100vh',
-                border: 'none',
-            }}
-        ></iframe>
-    );
+  return (
+    <iframe
+      src={resume}
+      title="Julian Hecker Web Development Resume"
+      width="100%"
+      style={{
+        display: 'block',
+        height: '100vh',
+        border: 'none',
+      }}
+    ></iframe>
+  );
 }
 
 export default Resume;
